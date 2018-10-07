66087f854dcfd9e261746b15bb53783c0a28f4aa
Fotis Kapotos <fotiskapotos@gmail.com>
2018-10-07 21:28:05 +0200

    Bug Fix

--- Arduino/code_arduino_test_1/code_arduino_test_1.ino
+++ Arduino/code_arduino_test_1/code_arduino_test_1.ino
@@ -6,6 +6,7 @@ float distance;
 
 Servo monServo;
 int positionDuServo = 0;
+int angle = 90;
 
 void setup() {
   pinMode(pinTrig, OUTPUT);
@@ -28,10 +29,12 @@ void loop() {
 
   if (temps < x) // si la distance est inférieure à x (à modifier) le moteur fait un demi tour
   {              
-    for (positionDuServo = 0; positionDuServo <= 180; positionDuServo ++)
+    /*for (positionDuServo = 0; positionDuServo <= 180; positionDuServo ++)
     { 
     monServo.write(positionDuServo);
     delay(15);
+    */
+    monServo.write(angle);
     }
   }
 
