e426d848426b6c47b9ad5590b649fc8485d32a25
RaphaPMLT <43858940+RaphaPMLT@users.noreply.github.com>
2018-10-07 21:07:30 +0200

    test Arduino #1
    code permettant d'ouvrir une porte suite à une anomalie du capteur ultrason

---
+++ code_arduino_test_1.ino
@@ -0,0 +1,42 @@
+#include <Servo.h>
+int pinTrig = 2;
+int pinEcho = 3;
+long temps;
+float distance;
+
+Servo monServo;
+int positionDuServo = 0;
+
+void setup() {
+  pinMode(pinTrig, OUTPUT);
+  pinMode(pinEcho, INPUT);
+  digitalWrite(pinTrig, LOW);  
+  Serial.begin(9600);
+  monServo.attach(9);
+  monServo.write(positionDuServo);
+}
+
+
+
+void loop() {
+
+  digitalWrite(pinTrig, HIGH);        
+  delayMicroseconds(10);
+  digitalWrite(pinTrig, LOW);
+
+  temps = pulseIn(pinEcho, HIGH);    
+
+  if (temps < x) // si la distance est inférieure à x (à modifier) le moteur fait un demi tour
+  {              
+    for (positionDuServo = 0; positionDuServo <= 180; positionDuServo ++)
+    { 
+    monServo.write(positionDuServo);
+    delay(15);
+    }
+  }
+
+  else {
+    
+  }
+  
+}
