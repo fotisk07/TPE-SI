32c877894fe6ad4f488d241628b976056af61855
Fotis Kapotos <fotiskapotos@gmail.com>
2019-01-17 09:41:24 +0100

    Serial Communication
    Arduino --> Rasberry
    Rasberry --> Arduino

---
+++ Arduino/arduino_message_raspberry.ino/arduino_message_raspberry.ino.ino
@@ -0,0 +1,31 @@
+char receivedChar;
+boolean newData = false;
+char message = "";
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(2, OUTPUT);
+}
+
+void loop() {
+  delay(1000);
+  
+}
+
+
+
+
+void recvInfo() {
+  if (Serial.available() > 0) {
+    receivedChar = Serial.read();
+    newData = true;
+    }
+}
+
+void sendInfo() {
+  Serial.println(message);
+  delay(100);
+}
+
+
