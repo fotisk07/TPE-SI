9e9725f191b32abf3652239f13496ce3174b0d9c
leranydu16 <43842126+leranydu16@users.noreply.github.com>
2019-01-22 18:00:36 +0100

    need this

---
+++ Arduino/sketqsdfqsdfqsdhlfksqjdfhqsdljkfhqslkdjfhsqdljkfhqsdkljfhsdqklj/sketqsdfqsdfqsdhlfksqjdfhqsdljkfhqslkdjfhsqdljkfhqsdkljfhsdqklj.ino
@@ -0,0 +1,44 @@
+#include <Servo.h>
+
+Servo Servo1;
+char receivedChar;
+boolean newData = false;
+char message = "GO";
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(4,INPUT);
+  0Servo1.attach(7);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  if (digitalRead(4) == HIGH) {
+    sendInfo();
+  }
+  recvInfo();
+  if (newData == true){
+    Servo1.write(90);
+  }
+  receivedChar = "";
+  boolean newData = false;
+  
+}
+
+
+void recvInfo() {
+  if (Serial.available() > 0) {
+    receivedChar = Serial.read();
+    newData = true;
+    }
+}
+
+void sendInfo() {
+  Serial.println(message);
+  delay(100);
+}
+
+
+
