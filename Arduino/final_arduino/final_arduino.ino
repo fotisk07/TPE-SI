fecc9608d01f2d5cd741ebd16386ed34f351332a
leranydu16 <43842126+leranydu16@users.noreply.github.com>
2019-01-31 09:51:45 +0100

    arduino important

--- Arduino/final_arduino/final_arduino.ino
+++ Arduino/final_arduino/final_arduino.ino
@@ -1,8 +1,11 @@
 #include <Servo.h>
 int newData = false ;
 char receivedChar = "" ;
-char message[5] = "No" ;
-int servo[3] = [9,8,7,6];
+char message[6] = "HE" ;
+int servo[4] = {9,8,7,6};
+
+Servo entree1;
+Servo sortie1;
 
 
 void setup() {
@@ -21,28 +24,18 @@ void loop() {
   // put your main code here, to run repeatedly:
   if (digitalRead(4) == HIGH)
   {
-    entree1.write(180);
-    delay(5000);
-    entree1.write(0);
-    while true
+    ouvrir_entree();
+    while (true)
     {
-      message = "Hello";
+      message[6] = "Hello";
       sendInfo();
       recvInfo();
-      if newData = true
+      if (newData == true)
       {
-        if receivedChar == "Yes"
-        {
-          sortie1.write(180);
-          delay(5000);
-          sortie1.write(0);
-        }
+        if (receivedChar == "Yes")
+          ouvrir_sortie();
         else
-        {
-          entree1.write(180);
-          delay(5000);
-          entree1.write(0);
-        }
+          ouvrir_entree();
       }
     }
     
@@ -63,4 +56,19 @@ void sendInfo() {
   Serial.println(message);
   delay(100);
 }
+
+void ouvrir_entree() 
+{
+    entree1.write(180);
+    delay(5000);
+    entree1.write(0);
+}
+
+void ouvrir_sortie()
+{
+  sortie1.write(180);
+  delay(5000);
+  sortie1.write(0);
+}
+
 
